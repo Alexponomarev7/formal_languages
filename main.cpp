2019e218028c9c880840890da1e98ddf977493b7
Alex Ponomarev <Alex.ponomarev7@ya.ru>
2018-12-01 11:47:32 +0300

    Fix min to max

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     std::cin >> s;
 
     lib::NFA nfa = lib::NFA(s);
-    std::cout << nfa.description << std::endl;
+    //std::cout << nfa.description << std::endl;
 
     std::cin >> p;
     size_t n = p.length();
@@ -15,9 +15,6 @@ int main() {
     int ans = 0;
     for (size_t i = 0; i < n; ++i) {
         nfa.clear();
-        if (nfa.is_terminal()) {
-            ans = 0;
-        }
 
         for (size_t j = i; j < n; ++j) {
             nfa.go(p[j]);
